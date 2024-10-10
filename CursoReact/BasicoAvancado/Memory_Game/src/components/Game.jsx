5b40f293b102415d36f2de572d8433a4925070ba
Kassiano-Santos <kassiano.santos122@gmail.com>
2024-10-10 02:37:42 -0300

    Add comment

--- CursoReact/BasicoAvancado/Memory_Game/src/components/Game.jsx
+++ CursoReact/BasicoAvancado/Memory_Game/src/components/Game.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Board from './Board';
 
- //Método para embaralhar o baralho
+ //Method for shuffling the deck
  const shuffleArray= (array)=> {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
