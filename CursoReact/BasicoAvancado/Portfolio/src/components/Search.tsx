61d2c85b954550792f82d14847c310b299f1e10c
Kassiano-Santos <kassiano.santos122@gmail.com>
2024-11-01 21:44:29 -0300

    Bug fix GitHub Finder

--- CursoReact/BasicoAvancado/Portfolio/src/components/Search.tsx
+++ CursoReact/BasicoAvancado/Portfolio/src/components/Search.tsx
@@ -2,7 +2,8 @@ import { useState, KeyboardEvent } from 'react';
 import styles from "./Search.module.css";
 import SearchIcon from '@mui/icons-material/Search';
 import Grid from '@mui/material/Grid2';
-import { Link } from '@mui/material';
+import { Container, Link } from '@mui/material';
+import { Height } from '@mui/icons-material';
 
 type SearchProps = {
   loadUser: (userName:string)=> Promise<void>;
@@ -19,27 +20,32 @@ const Search = ({loadUser}: SearchProps)=> {
   }
 
   return (
-    <div className={styles.search}>
-      <div className={styles.text}>
-        <h2>Search Users</h2>
-        <p>Discover your best projects</p>
-      </div>
-      <Grid className={styles.search_conteiner}>
-        <Grid>
-          <input 
-          type="text" 
-          placeholder="Enter username"
-          onChange={(e)=> setUserName(e.target.value)}
-          onKeyDown={handleKeyDown}
-        />
+    <Container maxWidth={false} sx={{Height: "100%", backgroundColor: "#0a192f"}}>
+        <Grid className={styles.search} container spacing={2}>
+          <Grid className={styles.text}>
+            <h2>Search Users</h2>
+            <p>Discover your best projects</p>
+          </Grid>
+          <Grid className={styles.search_conteiner}>
+            <Grid>
+              <input 
+              type="text" 
+              placeholder="Enter username"
+              onChange={(e)=> setUserName(e.target.value)}
+              onKeyDown={handleKeyDown}
+            />
+            </Grid>
+            <Grid>
+              <Link onClick={()=> loadUser(userName)} >
+                <SearchIcon className={styles.icon} sx={{ fontSize: 35 }}/>
+              </Link>
+            </Grid>
+          </Grid>
+          
         </Grid>
-        <Grid>
-          <Link onClick={()=> loadUser(userName)} >
-            <SearchIcon className={styles.icon} sx={{ fontSize: 35 }}/>
-          </Link>
-        </Grid>
-      </Grid>
-    </div>
+        <Grid className= {styles.baseboardSearch}>
+          </Grid>
+    </Container>
   )
 }
 export default Search;
