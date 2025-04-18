8e165dc071c219fe1a158ab0975a5d5781c7f97f
Kassiano-Santos <kassiano.santos122@gmail.com>
2025-04-18 06:40:56 -0300

    Creating the front end for the password generator with Java Secure Random

--- JavaProjects/GenerateSecurityPasswordProject/src/GenerateSecurityPassword.java
+++ JavaProjects/GenerateSecurityPasswordProject/src/GenerateSecurityPassword.java
@@ -1,7 +1,19 @@
+
+
 import java.security.SecureRandom;
 import java.util.Scanner;
-
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+
 import javafx.stage.Stage;
 
 public class GenerateSecurityPassword extends Application{
@@ -10,7 +22,30 @@ public class GenerateSecurityPassword extends Application{
 	
 	@Override
 	public void start(Stage stage) {
+		stage.setTitle("Password Generator");
 		
+		Label labelPassSize = new Label("Password Size");
+		TextField passSizeField = new TextField();
+		passSizeField.setText("8");
+		
+		Label passGenerated = new Label("Password Generated: ");
+		TextField passGenField = new TextField();
+		passGenField.setEditable(false);
+		
+		Button passGenbutton = new Button("Generate Password");
+		passGenbutton.setOnAction(e -> {
+			int passSize = Integer.parseInt(passSizeField.getText());
+			String password = generatePassword(passSize);
+			passGenField.setText(password);
+		});
+		
+		VBox vBox = new VBox (labelPassSize, passSizeField, passGenbutton, passGenerated,  passGenField);
+		vBox.setSpacing(10);
+		vBox.setPadding(new Insets(10));
+				
+		Scene scene = new Scene(vBox, 250,200);
+		stage.setScene(scene);
+		stage.show();
 	}
 	
 	public static String generatePassword(int lengthed){
@@ -25,11 +60,14 @@ public class GenerateSecurityPassword extends Application{
 	}
 
 	public static void main(String[] args) {
+		/*
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter the number of characters you want to create the password: ");
 		int lengthedPassword = scanner.nextInt();
 		String password = generatePassword(lengthedPassword);
 		System.out.println("Password generated: " + password);
+		*/
+		launch(args);
 	}
 
 }
